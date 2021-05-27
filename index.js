1b3e20dc69faa578e880d6d12f09641df83a148a
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2021-05-28 01:18:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let readlineSync = require("readline-sync");
+
+let quiz = [
+  {
+  question: "Who is my favorite Marvel superhero?",
+  answer: "Doctor Strange"
+},
+  {
+  question: "What is my favorite youtube channel?",
+  answer: "TED ED"
+},
+  {
+  question: "Who is my favorite DC superhero?",
+  answer: "Wonder Woman"
+},
+  {
+  question: "What is my favourite anime?",
+  answer: "Castlevania"
+},
+  {
+  question: "What is my favourite street food?",
+  answer: "Shawarma"
+},
+  {
+  question: "What is my favorite cuisine?",
+  answer: "Mexican"
+},
+  {
+  question: "Which do i like more Genshin or Valorant?",
+  answer: "Valorant"
+},
+  {
+  question: "Who is my favourite music artist?",
+  answer: "Billie Eilish"
+},
+]
+
+let highscores = [
+  {
+    name: "random1",
+    score: 14
+  },
+  {
+    name: "random2",
+    score: 16
+  },
+]
+
